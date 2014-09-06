025a040fb368a6589b84a8bcfc47fdb9e7e0b124
Marco Boekholt <marco.boekholt@hotmail.com>
2014-09-07 01:02:39 +0200

    added itemstack with costum displayname and soulbound lore.

---
+++ src/me/marcoboekholt/godlike/data/items.java
@@ -0,0 +1,32 @@
+package me.marcoboekholt.godlike.data;
+
+import java.util.ArrayList;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class items {
+		
+	public void sword(Player p, String pname) {
+		ItemStack swordi = new ItemStack(Material.DIAMOND_SWORD, 1);
+		ItemMeta swordmeta = swordi.getItemMeta();
+		swordmeta.setDisplayName(ChatColor.GREEN + pname + "`s" + ChatColor.DARK_GRAY + " sword");
+		ArrayList<String> sl = new ArrayList<String>();
+		sl.add(ChatColor.DARK_PURPLE + "Godlike sword");
+		sl.add("");
+		sl.add(ChatColor.YELLOW + "Soulbound");
+		swordmeta.setLore(sl);
+		swordi.setItemMeta(swordmeta);
+		swordi.addEnchantment(Enchantment.KNOCKBACK, 2);
+		swordi.addEnchantment(Enchantment.DAMAGE_ALL, 5);
+		swordi.addEnchantment(Enchantment.FIRE_ASPECT, 2);
+		PlayerInventory pl = p.getInventory();
+		pl.addItem(swordi);
+	}
+
+}
