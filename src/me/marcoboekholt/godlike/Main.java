3b111c8044f34916ff725b0453ca48f4532494a6
Marco Boekholt <marco.boekholt@hotmail.com>
2014-09-06 20:13:25 +0200

    Uploaded Godlike project

---
+++ src/me/marcoboekholt/godlike/Main.java
@@ -0,0 +1,15 @@
+package me.marcoboekholt.godlike;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+
+}
