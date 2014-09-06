01119566bb99a0339af1d45426590658a643214b
Marco Boekholt <marco.boekholt@hotmail.com>
2014-09-06 20:30:11 +0200

    Added some comments and a TODO

--- src/me/marcoboekholt/godlike/Main.java
+++ src/me/marcoboekholt/godlike/Main.java
@@ -2,14 +2,20 @@ package me.marcoboekholt.godlike;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+//Waiting on the topic of bukkit what people want
+//url: http://forums.bukkit.org/threads/godlike.309970/
+//please leave your comment and let me know what you want
+//Author: Marco Boekholt
+//Plugin name: Godlike
+
 public class Main extends JavaPlugin {
 	
 	public void onEnable() {
-		
+		//Some stuff here
 	}
 	
 	public void onDisable() {
-		
+		//Some stuff here
 	}
 
 }
---
+++ src/me/marcoboekholt/godlike/commands/commands.java
@@ -0,0 +1,16 @@
+package me.marcoboekholt.godlike.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class commands implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		//TODO add commands here
+		return false;
+	}
+
+}
